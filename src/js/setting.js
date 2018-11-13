554c26d80dd0dcc75384a487b8cdb4f9cd4c1dfc
lazaryan <lazaryan99@gmail.com>
2018-11-13 23:04:17 +0300

    add images for buttons ans add setting style text

--- src/js/setting.js
+++ src/js/setting.js
@@ -5,31 +5,43 @@
 function Setting () {
 	this.el = undefined;
 	this.nav = undefined;
+
 	this.buttons = {
 		changeText: {
 			message: 'Редактировать текст',
+			label: './img/edit.svg',
 			action: this.changeText.bind(this)
+		},
+		changeStyleText: {
+			message: 'Изменить стили текста',
+			label: './img/paint-brush.svg',
+			action: this.changeStyleText.bind(this)
 		}
 	}
 }
 
 Setting.prototype = {
-	init(el) {
+	_changeStyle: false,
+	_settingText: [
+		'font-size',
+		'color'
+	],
+	init (el) {
 		this.el = el ? el : this.el;
 
 		this.addMenu();
 
 		return this.el;
 	},
-	addMenu() {
+	addMenu () {
 		this.nav = this.createBlock();
 		this.el.insertBefore(this.nav, this.el.firstChild);
 	},
-	removeMenu() {
+	removeMenu () {
 		this.el.removeChild(this.el.firstChild);
 		this.nav = undefined;
 	},
-	createBlock() {
+	createBlock () {
 		let block = document.createElement('div');
 		block.classList = 'el__action-setting';
 		
@@ -39,14 +51,17 @@ Setting.prototype = {
 
 		return block;
 	},
-	createButtons(name, action) {
+	createButtons (name, action) {
 		let block = document.createElement('div');
 		block.classList.add('bth_button');
-		block.style.display = 'inline-block';
-		block.innerHTML = 'Изменить';
 		block.dataset.name = name;
-		block.dataset.typeName = 'setting';
 		block.addEventListener('click', (e) => action(e));
+
+		let img = document.createElement('img');
+		img.src = this.buttons[name].label;
+		img.classList.add('bth_button_img');
+
+		block.appendChild(img);
 
 		return block;
 	},
@@ -56,6 +71,35 @@ Setting.prototype = {
 			this.el.setAttribute('contentEditable', 'true');
 		} else {
 			this.el.setAttribute('contentEditable', edit == 'true' ? 'false' : 'true');
+		}
+	},
+	changeStyleText () {
+		this._changeStyle = !this._changeStyle;
+
+		if(this._changeStyle) {
+			let template = document.createElement('div');
+			template.classList = 'setting_style-text';
+			template.innerHTML = 
+			`
+				<div>
+					${this._settingText.reduce((s, e) => {
+						s += `<p><label>${e}: </label><input type="text" /></p>`;
+
+						return s;
+					}, '')}
+				</div>
+			`;
+
+			this.nav.appendChild(template);
+		} else {
+			let style = this.nav.querySelectorAll('input[type="text"]');
+
+			for(let i = 0; i < style.length; i++) {
+				if(style[i].value) {
+					this.el.style[this._settingText[i]] = style[i].value;
+				}
+			}
+			this.nav.removeChild(this.nav.lastChild);
 		}
 	}
 }
