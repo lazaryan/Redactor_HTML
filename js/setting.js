45e71e431b5cd9ff7e22ab77f4353557733596f3
lazaryan <lazaryan99@gmail.com>
2018-11-13 23:01:18 +0300

    add images for buttons ans add setting style text

--- js/setting.js
+++ js/setting.js
@@ -7,15 +7,24 @@
 function Setting() {
 	this.el = undefined;
 	this.nav = undefined;
+
 	this.buttons = {
 		changeText: {
 			message: 'Редактировать текст',
+			label: './img/edit.svg',
 			action: this.changeText.bind(this)
+		},
+		changeStyleText: {
+			message: 'Изменить стили текста',
+			label: './img/paint-brush.svg',
+			action: this.changeStyleText.bind(this)
 		}
 	};
 }
 
 Setting.prototype = {
+	_changeStyle: false,
+	_settingText: ['font-size', 'color'],
 	init: function init(el) {
 		this.el = el ? el : this.el;
 
@@ -44,13 +53,16 @@ Setting.prototype = {
 	createButtons: function createButtons(name, action) {
 		var block = document.createElement('div');
 		block.classList.add('bth_button');
-		block.style.display = 'inline-block';
-		block.innerHTML = 'Изменить';
 		block.dataset.name = name;
-		block.dataset.typeName = 'setting';
 		block.addEventListener('click', function (e) {
 			return action(e);
 		});
+
+		var img = document.createElement('img');
+		img.src = this.buttons[name].label;
+		img.classList.add('bth_button_img');
+
+		block.appendChild(img);
 
 		return block;
 	},
@@ -60,6 +72,30 @@ Setting.prototype = {
 			this.el.setAttribute('contentEditable', 'true');
 		} else {
 			this.el.setAttribute('contentEditable', edit == 'true' ? 'false' : 'true');
+		}
+	},
+	changeStyleText: function changeStyleText() {
+		this._changeStyle = !this._changeStyle;
+
+		if (this._changeStyle) {
+			var template = document.createElement('div');
+			template.classList = 'setting_style-text';
+			template.innerHTML = '\n\t\t\t\t<div>\n\t\t\t\t\t' + this._settingText.reduce(function (s, e) {
+				s += '<p><label>' + e + ': </label><input type="text" /></p>';
+
+				return s;
+			}, '') + '\n\t\t\t\t</div>\n\t\t\t';
+
+			this.nav.appendChild(template);
+		} else {
+			var style = this.nav.querySelectorAll('input[type="text"]');
+
+			for (var i = 0; i < style.length; i++) {
+				if (style[i].value) {
+					this.el.style[this._settingText[i]] = style[i].value;
+				}
+			}
+			this.nav.removeChild(this.nav.lastChild);
 		}
 	}
 };
